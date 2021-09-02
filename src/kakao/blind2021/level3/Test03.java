52a921d627171598290aacd4ff25b6d3eefcc5a8
김초원 <zzzcc33@naver.com>
2021-09-03 03:21:14 +0900

    카드 짝 맞추기

--- src/kakao/blind2021/level3/Test03.java
+++ src/kakao/blind2021/level3/Test03.java
@@ -19,114 +19,97 @@ public class Test03 {
         System.out.println(answer);
     }
 
-    static class Card {
-        int value;
+    static class Point {
         int x;
         int y;
         int count;
-
-        public Card(int value, int x, int y, int count) {
-            this.value = value;
-            this.x = x;
-            this.y = y;
-            this.count = count;
-        }
-
-        public Card(int x, int y, int count) {
+        public Point(int x, int y, int count) {
             this.x = x;
             this.y = y;
             this.count = count;
         }
     }
 
-    public static List<Card> cardList;
     public static int[][] BOARD = new int[4][4];
     public static int ANSWER = Integer.MAX_VALUE;
     public static int R;
     public static int C;
 
     public static int solution(int[][] board, int r, int c) {
-        int answer = 0;
-
-        init(board);
+        initBoard(board);
         R = r;
         C = c;
-        cardList = new ArrayList<>();
+        List<Point> cardList = new ArrayList<>();
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 if (board[i][j] > 0) {
-                    Card card = new Card(board[i][j], i, j, 0);
+                    Point card = new Point(i, j, 0);
                     cardList.add(card);
                 }
             }
         }
 
-        // 카드 값 순차로 정렬
-        cardList.sort(Comparator.comparingInt(o -> o.value));
-
-        Card[] item = new Card[cardList.size()];
-        for (int i = 0; i < cardList.size(); i += 2) {
-            Card card1 = cardList.get(i);
-            item[i] = new Card(card1.value, card1.x, card1.y, 0);
-
-            Card card2 = cardList.get(i + 1);
-            item[i + 1] = new Card(-card2.value, card2.x, card2.y, 0);
-        }
-
-        Card[] bucket = new Card[item.length];
-        permutation(item, bucket, bucket.length, board);
-
+        Point[] bucket = new Point[cardList.size()];
+        permutation(cardList, bucket, bucket.length, board);
         return ANSWER + cardList.size();
     }
 
+    public static void initBoard(int[][] board) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                BOARD[i][j] = board[i][j];
+            }
+        }
+    }
 
-    public static int order = 0;
-
-    public static void permutation(Card[] items, Card[] bucket, int k, int[][] board) {
+    public static void permutation(List<Point> pointList, Point[] bucket, int k, int[][] board) {
         if (k == 0) {
             int total = 0;
-            Card initCursor = new Card(R, C, 0);
-            Card start = bucket[0];
-            total += getNearestPath(initCursor, start);
+            Point start = new Point(R, C, 0);
+            Point end = bucket[0];
+            total += getNearestPath(start, end);
             BOARD[start.x][start.y] = 0;
 
             for (int i = 0; i < bucket.length - 1; i++) {
                 start = bucket[i];
-                Card end = bucket[i + 1];
-                int nearestPath = getNearestPath(start, end);
+                end = bucket[i + 1];
+                total += getNearestPath(start, end);
                 BOARD[start.x][start.y] = 0;
                 BOARD[end.x][end.y] = 0;
-                total += nearestPath;
             }
             ANSWER = Math.min(ANSWER, total);
-            init(board);
+            initBoard(board);
             return;
         }
 
         int lastIndex = bucket.length - k - 1;
-        for (int i = 0; i < items.length; i++) {
+        for (int i = 0; i < pointList.size(); i++) {
             if (bucket.length == k) {
-                bucket[0] = items[i];
-                permutation(items, bucket, k - 1, board);
+                bucket[0] = pointList.get(i);
+                permutation(pointList, bucket, k - 1, board);
             } else {
                 // 이번에 넣을 인덱스가 짝수번째이면
                 if ((lastIndex + 1) % 2 == 0) {
                     boolean isSame = false;
                     for (int j = 0; j <= lastIndex; j++) {
-                        if (bucket[j].value == items[i].value) {
+                        if (bucket[j].x == pointList.get(i).x && bucket[j].y == pointList.get(i).y) {
                             isSame = true;
                             break;
                         }
                     }
 
                     if (!isSame) {
-                        bucket[lastIndex + 1] = items[i];
-                        permutation(items, bucket, k - 1, board);
+                        bucket[lastIndex + 1] = pointList.get(i);
+                        permutation(pointList, bucket, k - 1, board);
                     }
                 } else { // 이번에 넣을 인덱스가 홀수번째이면
-                    if (bucket[lastIndex].value == -items[i].value) {
-                        bucket[lastIndex + 1] = items[i];
-                        permutation(items, bucket, k - 1, board);
+                    Point last = bucket[lastIndex];
+                    Point cur = pointList.get(i);
+                    if (BOARD[last.x][last.y] == BOARD[cur.x][cur.y]) {
+                        if (bucket[lastIndex].x != pointList.get(i).x || bucket[lastIndex].y != pointList.get(i).y) {
+                            bucket[lastIndex + 1] = pointList.get(i);
+                            permutation(pointList, bucket, k - 1, board);
+                        }
                     }
                 }
             }
@@ -135,55 +118,44 @@ public class Test03 {
 
     public static int[] moveX = {-1, +1, 0, 0};
     public static int[] moveY = {0, 0, -1, +1};
-    public static int getNearestPath(Card start, Card end) {
+    public static int getNearestPath(Point start, Point end) {
         boolean[][] visited = new boolean[4][4];
-        Queue<Card> queue = new LinkedList<>();
+        Queue<Point> queue = new LinkedList<>();
         queue.add(start);
         int count = 0;
 
         while (!queue.isEmpty()) {
-            Card cur = queue.poll();
-
-            if (BOARD[cur.x][cur.y] > 0) {
-                visited[cur.x][cur.y] = true;
-            }
+            Point cur = queue.poll();
+            visited[cur.x][cur.y] = true;
 
             if (cur.x == end.x && cur.y == end.y) {
-                count = cur.count;
-                break;
+                return cur.count;
             }
 
-            // 상하좌우 한칸씩 이동
             for (int i = 0; i < 4; i++) {
+                // 상하좌우 한칸씩 이동
                 int x = cur.x + moveX[i];
                 int y = cur.y + moveY[i];
-
-                if (validationCheckCoordinate(x, y)) {
-                    if (!visited[x][y]) {
-                        queue.add(new Card(x, y, cur.count + 1));
-                    }
+                if (validationCheckCoordinate(x, y) && !visited[x][y]) {
+                    queue.add(new Point(x, y, cur.count + 1));
                 }
-            }
 
-            // ctrl + 상하좌우 이동
-            for (int i = 0; i < 4; i++) {
-                Card nearestCard = getNearestCard(cur, moveX[i], moveY[i]);
-                int x = nearestCard.x;
-                int y = nearestCard.y;
-                if (validationCheckCoordinate(x, y)) {
-                    if (!visited[x][y]) {
-                        queue.add(new Card(x, y, cur.count + 1));
-                    }
+                // ctrl + 상하좌우 이동
+                Point nearestCard = getNearestCard(cur, moveX[i], moveY[i]);
+                x = nearestCard.x;
+                y = nearestCard.y;
+                if (validationCheckCoordinate(x, y) && !visited[x][y]) {
+                    queue.add(new Point(x, y, cur.count + 1));
                 }
             }
         }
         return count;
     }
 
-    public static Card getNearestCard(Card cur, int moveX, int moveY) {
+    public static Point getNearestCard(Point cur, int moveX, int moveY) {
         int x = cur.x;
         int y = cur.y;
-        Card nearestCard = new Card(x, y, 0);
+        Point nearestCard = new Point(x, y, 0);
 
         while (true) {
             x += moveX;
@@ -200,7 +172,6 @@ public class Test03 {
                 return nearestCard;
             }
         }
-
         return nearestCard;
     }
 
@@ -209,13 +180,5 @@ public class Test03 {
             return false;
         }
         return true;
-    }
-
-    public static void init(int[][] board) {
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                BOARD[i][j] = board[i][j];
-            }
-        }
     }
 }
